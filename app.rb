855cf48cfee13af85d9cec42c7152090fa9d59f8
Savannah C <sgcorrell@gmail.com>
2019-06-12 20:05:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,8 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get "/reversename/:name" do
-    @reverse_name = params[:name]
+    @reverse_name = params[:name].reverse
+    @reverse_name
   end
 
 end
