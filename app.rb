d5449d98589cf43e16ad5d0dd6ed8057df6851df
Savannah C <sgcorrell@gmail.com>
2019-06-12 20:09:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -28,5 +28,9 @@ class App < Sinatra::Base
     @word5 = params[:word5]
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}."
   end
+  
+  get "/:operation/:number1/:number2" do
+  
+  end
 
 end
