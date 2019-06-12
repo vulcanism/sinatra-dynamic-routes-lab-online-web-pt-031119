c2f1d04bd673684a15fef759893d1867b9e1711b
Savannah C <sgcorrell@gmail.com>
2019-06-12 20:30:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,14 +20,14 @@ class App < Sinatra::Base
     string
   end
   
-  get "get /say/:word1/:word2/:word3/:word4/:word5" do
+  get "/say/:word1/:word2/:word3/:word4/:word5" do 
     @word1 = params[:word1]
     @word2 = params[:word2]
     @word3 = params[:word3]
     @word4 = params[:word4]
     @word5 = params[:word5]
-    "#{@word1} + #{@word2} + #{@word3} + #{@word4} + #{@word5}."
-  end
+    "#{@word1} " + "#{@word2} " + "#{@word3} " + "#{@word4} " + "#{@word5}."
+  end 
   
   get "/:operation/:number1/:number2" do
     @operation = params[:operation]
