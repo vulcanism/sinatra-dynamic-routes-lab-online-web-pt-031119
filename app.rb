c48b4172502bbcd080b30116bc0f82c8438fd398
Savannah C <sgcorrell@gmail.com>
2019-06-12 20:10:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -30,7 +30,19 @@ class App < Sinatra::Base
   end
   
   get "/:operation/:number1/:number2" do
-  
+    @oper = params[:operation]
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
+    case @oper
+      when "subtract"
+        (@num1 - @num2).to_s
+      when "add"
+        (@num1 + @num2).to_s
+      when "multiply"
+        (@num1 * @num2).to_s
+      when "divide"
+        (@num1 / @num2).to_s
+    end
   end
 
 end
