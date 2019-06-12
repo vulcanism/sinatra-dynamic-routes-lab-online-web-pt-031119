445c6762eac030833bdc6be87c4d44915f029468
Savannah C <sgcorrell@gmail.com>
2019-06-12 20:10:27 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ class App < Sinatra::Base
   end
   
   get "/:operation/:number1/:number2" do
-    @oper = params[:operation]
+    @operation = params[:operation]
     @num1 = params[:number1].to_i
     @num2 = params[:number2].to_i
     case @oper
